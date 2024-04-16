145c8ee9ab9a402fde1fc3966a0781460eecf0a9
Youness <129855046+ys218279@users.noreply.github.com>
2024-04-16 10:41:55 +0100

    Updated License Finder Script
    Updated findJson function, removed find filePaths function

--- callbackTest.ts
+++ callbackTest.ts
@@ -1,15 +1,31 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
 
-const array: string[] = [
-    '/Users/yousen01/Arm/ksc-ide/node_modules/@webpack-cli',
-    '/Users/yousen01/Arm/ksc-ide/node_modules/@xtuc',
-    '/Users/yousen01/Arm/ksc-ide/node_modules/JSONStream',
-    '/Users/yousen01/Arm/ksc-ide/node_modules/abab',
-    '/Users/yousen01/Arm/ksc-ide/node_modules/.bin'
-];
+if (process.argv.length < 3) {
+    console.error('Usage: ts-node script.ts <startingPath>');
+    process.exit(1);
+}
 
+// Get the starting path from the command-line arguments
+const startingPath = process.argv[2];
 
+function goToNodeModules(startingPath: string, callback: (filePaths: string[]) => void) {
+    console.log("start function")
+    const filePaths: string[] = []
+    const dir: string = path.join(startingPath, "node_modules");
+    fs.readdir(dir, (err: NodeJS.ErrnoException | null, files: string[]) => {
+        if (err) {
+            console.error(err);
+            return; // Exit early if there's an error
+        }
+        files.forEach((file) => {
+            filePaths.push(path.join(dir, file));
+        });
+        console.log(filePaths); // Log the populated filePaths array
+        callback(filePaths);
+    });
+}
 function validating(filePaths: string[], callback: (processedFiles: string[]) => void) {
     console.log("validating");
     const processedFiles: string[] = [];
@@ -47,128 +63,63 @@ function validating(filePaths: string[], callback: (processedFiles: string[]) =>
 function findFilePaths(processedFiles: string[], callback: (filePathList: string[]) => void) {
     console.log('collecting filePaths');
     const filePathList: string[] = [];
-    const authors: string[] = [];
-    let pendingReads = processedFiles.length;
+    let pendingReads: number = processedFiles.length;
 
     processedFiles.forEach((filePath) => {
         fs.readdir(filePath, (err: NodeJS.ErrnoException | null, files: string[]) => {
             if (err) {
                 console.error(err);
-                --pendingReads;
-            }
-
-            let pendingStats: number = files.length || 0;
-
-            files.forEach((file) => {
-                let fullPath = path.join(filePath, file);
-
-                fs.stat(fullPath, (err, stats) => {
-                    if (err) {
-                        console.error(err);
-                        return; // Return early if there's an error
-                    }
-
-                    if (stats.isDirectory()) {
-                        authors.push(fullPath);
-                    }
-                    filePathList.push(fullPath);
-                    pendingStats--;
-
-                    if (pendingStats === 0) {
-                        // Check if there are authors to traverse
-                        if (authors.length > 0) {
-                            authors.forEach((filePathA) => {
-                                fs.readdir(filePathA, (err: NodeJS.ErrnoException | null, nestedFilesA: string[]) => {
-                                    if (err) {
-                                        console.error(err);
-
-                                    }
-                                    nestedFilesA.forEach((nestedFile) => {
-                                        filePathList.push(path.join(filePathA, nestedFile));
-                                    });
-                                });
-                            });
-                        }
-                        pendingReads--;
-                        if (pendingReads === 0) {
-                            //console.log('found all filePaths');
-                            //console.log(filePathList);
-                            callback(filePathList);
-                        }
-                    }
-                });
-            });
-        });
-
-    });
-}
-
-function findJSON(filePathList: string[], callback: (jsonFiles: string[]) => void) {
-    console.log('finding JSON files');
-    const jsonFiles: string[] = [];
-    let pendingReads: number = filePathList.length;
-
-    filePathList.forEach((filePath) => {
-        if (filePath.endsWith('package.json')) {
-            console.log(filePath);
-            jsonFiles.push(filePath);
-            pendingReads--;
-            if (pendingReads === 0) {
-                callback(jsonFiles);
-            }
-        }
-        else {
-            fs.stat(filePath, (err, stats) => {
-                //console.log("stats")
-                if (err) {
-                    console.error(err);
-                    pendingReads--;
-                    if (pendingReads === 0) {
-                        callback(jsonFiles);
-                    }
-                    return;
+                pendingReads--;
+                if (pendingReads === 0) {
+                    callback(filePathList);
                 }
+                return;
+            }
 
-                if (stats.isDirectory()) {
-                    fs.readdir(filePath, (err: NodeJS.ErrnoException | null, files: string[]) => {
+            if (files.includes("package.json")) {
+                filePathList.push(path.join(filePath, 'package.json'));
+                pendingReads--;
+                if (pendingReads === 0) {
+                    callback(filePathList);
+                }
+            } else {
+                let pendingNestedReads: number = files.length;
+
+                files.forEach((file) => {
+                    const nestedFilePath: string = path.join(filePath, file);
+                    fs.readdir(nestedFilePath, (err: NodeJS.ErrnoException | null, nestedFiles: string[]) => {
                         if (err) {
                             console.error(err);
-                            pendingReads--;
-                            if (pendingReads === 0) {
-                                callback(jsonFiles);
-                            }
-                            return;
-                        }
-
-                        let pendingNestedReads: number = files.length;
-                        files.forEach((file) => {
-                            const fullPath: string = path.join(filePath, file);
-                            if (fullPath.endsWith('package.json')) {
-                                jsonFiles.push(fullPath);
-                            }
                             pendingNestedReads--;
                             if (pendingNestedReads === 0) {
                                 pendingReads--;
                                 if (pendingReads === 0) {
-                                    callback(jsonFiles);
+                                    callback(filePathList);
                                 }
                             }
-                        });
+                            return;
+                        }
+
+                        if (nestedFiles.includes('package.json')) {
+                            filePathList.push(path.join(nestedFilePath, 'package.json'));
+                        }
+
+                        pendingNestedReads--;
+                        if (pendingNestedReads === 0) {
+                            pendingReads--;
+                            if (pendingReads === 0) {
+                                callback(filePathList);
+                            }
+                        }
                     });
-                } else {
-                    pendingReads--;
-                    if (pendingReads === 0) {
-                        console.log(jsonFiles)
-                        callback(jsonFiles);
-                    }
-                }
-            });
-        }
+                });
+            }
+        });
     });
 }
 
-function findLicense(files: string[], callback: (data: { [key: string]: string }) => void) {
-    const data: { [key: string]: string } = {};
+function findLicense(files: string[], callback: (data: { [key: string]: string[] }) => void) {
+    const data: { [key: string]: string[] } = {};
     let filesProcessed = 0;
 
     files.forEach(file => {
@@ -180,14 +131,17 @@ function findLicense(files: string[], callback: (data: { [key: string]: string }
 
             const json = JSON.parse(content);
             if (json && json.license) {
-                data[file] = json.license;
+                const license = json.license;
+                if (!data[license]) {
+                    data[license] = [];
+                }
+                data[license].push(file);
             }
 
             filesProcessed++;
 
             // If all files have been processed, invoke the callback
             if (filesProcessed === files.length) {
-                //console.log("All files processed:", data);
                 callback(data);
             }
         });
@@ -195,32 +149,24 @@ function findLicense(files: string[], callback: (data: { [key: string]: string }
 }
 
 
-function writeToJSON(content: { [key: string]: string }, dir: string, fileName: string) {
+function writeToJSON(content: { [key: string]: string[] }, dir: string, fileName: string) {
     const filePath = path.join(dir, fileName);
     const objectContent = JSON.stringify(content);
     //console.log(objectContent)
     fs.writeFileSync(filePath, objectContent);
 };
 
-// Define a function to process the fetched data
-function processData(data: string[]) {
+// Define a function to process the fetched data. Used for debugging
+function processData(data:string[]) {
     console.log("Data processed:", data);
 }
-/*
-findFilePaths(array, (filePathList) => {
-    findJSON(filePathList, (jsonFiles) => {
-        processData(jsonFiles)
-    });
-});
 
-*/
-
-validating(array, (processedFiles) => {
-    findFilePaths(processedFiles, (filePathList) => {
-        findJSON(filePathList, (jsonFiles) => {
-            findLicense(jsonFiles, (data) => {
-                writeToJSON(data, '/Users/yousen01/Documents/Practice code/Federicco challenges/results', 'results.json')
+goToNodeModules(startingPath,(filePaths)=>{
+    validating(filePaths,(processFiles)=>{
+        findFilePaths(processFiles,(filePathsList)=>{
+            findLicense(filePathsList, (data) => {
+                writeToJSON(data, '<destination path>', 'results.json')
             })
-        });
-    });
+        })
+    })
 })
