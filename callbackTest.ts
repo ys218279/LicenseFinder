dc5adf68480c74a2003198dc9b66673d7b8a2936
Youness <129855046+ys218279@users.noreply.github.com>
2024-05-02 13:15:34 +0100

    Update callbackTest.ts
    results are now a relative path

--- callbackTest.ts
+++ callbackTest.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { stringify } from 'querystring';
 
 //argparse 
-if (process.argv.length < 4) {
-    console.error('Usage: ts-node script.ts <startingPath> <destinationPath>');
+if (process.argv.length <= 3) {
+    console.error('Usage: ts-node script.ts <startingPath>');
     process.exit(1);
 }
 
@@ -23,7 +24,7 @@ function goToNodeModules(startingPath: string, callback: (filePaths: string[]) =
         files.forEach((file) => {
             filePaths.push(path.join(dir, file));
         });
-        console.log(filePaths); 
+        console.log(filePaths); // Log the populated filePaths array
         callback(filePaths);
     });
 }
@@ -136,6 +137,7 @@ function findLicense(files: string[], callback: (data: { [key: string]: string[]
                 if (!data[license]) {
                     data[license] = [];
                 }
+
                 data[license].push(file);
             }
 
@@ -149,28 +151,63 @@ function findLicense(files: string[], callback: (data: { [key: string]: string[]
     });
 }
 
+function createRelativePath(content: { [key: string]: string[] }, callback: (relativePaths: { [key: string]: string[] }) => void) {
+    const modifiedContent: { [key: string]: string[] } = {};
+
+    for (const key in content) {
+        const values = content[key];
+        const modifiedValues: string[] = [];
+
+        for (let i = 0; i < values.length; i++) {
+            const directoryPath: string = path.dirname(values[i]); // strips filename
+            const splitDirs: string[] = directoryPath.split(path.sep); //splits into a list of elements
+            const lastTwoDirs: string[] = splitDirs.slice(-2);
+
+            for (let x = 0; x < lastTwoDirs.length; x++) {
+                if (lastTwoDirs[x] === "node_modules") {
+                    lastTwoDirs.splice(x, 1);
+                    x--; 
+                }
+            }
+
+            const relativePath = lastTwoDirs.join(path.sep);
+            modifiedValues.push(relativePath);
+        }
+
+        modifiedContent[key] = modifiedValues;
+    }
+
+    callback(modifiedContent);
+}
+
+
 
 function writeToJSON(content: { [key: string]: string[] }, dir: string, fileName: string) {
     const filePath = path.join(dir, fileName);
-    const objectContent = JSON.stringify(content);
-    fs.writeFile(filePath,objectContent,(err)=>{
-        if (err){
+    const objectContent = JSON.stringify((content));
+
+    fs.writeFile(filePath, objectContent, (err) => {
+        if (err) {
             console.log(err);
         }
-        console.log("File written successfully\n");
+        else {
+            console.log("File written successfully\n");
+        }
     })
 };
 
 // Define a function to process the fetched data. Used for debugging
-function processData(data: string[]) {
-    console.log("Data processed:", data);
+function processData(content: string[]) {
+    console.log("Data processed:", content);
 }
 
-goToNodeModules(startingPath, (filePaths) => {
+goToNodeModules(startingPath,(filePaths) => {
     validating(filePaths, (processFiles) => {
         findJsonFiles(processFiles, (filePathsList) => {
             findLicense(filePathsList, (data) => {
-                writeToJSON(data, destinationPath, 'results.json')
+                createRelativePath(data, (filteredContent) => {
+                    writeToJSON(filteredContent, destinationPath, 'results.json')
+                })
             })
         })
     })
